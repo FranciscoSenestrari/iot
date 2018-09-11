817302b976af467310c415737177e75afddde35c
Francisco Jose <fran.senes@gmail.com>
2018-09-11 17:22:00 -0300

    intento 2

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/', function (req,res) {
     res.send('hola');
 });
-app.listen(8080,function () {
-    console.log("escuchando ene el el http://localhost:8080")
+
+
+const PORT = process.env.PORT ||8080;
+app.listen(PORT,function () {
+    console.log("escuchando ene el el http://localhost:"+PORT)
 });
