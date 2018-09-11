b1b3466d94047364bcef9330a842f51f480208b6
Francisco Jose <fran.senes@gmail.com>
2018-09-11 18:14:34 -0300

    segundo

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const  express =  require('express') ;
 const app = express();
+let cont = 0;
 
 app.use(express.static("public"));
-app.get('/api/getUser', function (req,res) {
-    res.send('hola');
+app.get('/api/getM', function (req,res) {
+    res.send("#" + Math.round(Math.random()));
+    console.log(cont++);
 });
 
 
